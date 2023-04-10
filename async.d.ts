6c0766885425a4619adf5c8bd989529d7adbd022
BwehaaFox <Bwehaa@gmail.com>
2023-04-11 00:38:16 +0300

    Fix gor supporting ember-concurrency-async format

--- async.d.ts
+++ async.d.ts
@@ -15,9 +15,9 @@ type AsyncTaskFunction = GenericAsyncTaskFunction<any, any[]>;
 type AsyncDescriptor = GenericAsyncDescriptor<any, any[]>;
 
 declare module 'ember-concurrency-ts' {
-  function taskFor<T extends AsyncTaskFunction>(task: T): AsyncTaskFor<T>;
+  function taskFor<T extends AsyncTaskFunction>(task: T): AsyncTaskFor<T, T>;
   function taskFor<T extends AsyncDescriptor>(task: T): AsyncTaskForDescriptor<T>;
 
-  function perform<T extends AsyncTaskFunction>(task: T, ...args: Args<T>): AsyncInstanceFor<T>;
+  function perform<T extends AsyncTaskFunction>(task: T, ...args: Args<T>): AsyncInstanceFor<T, T>;
   function perform<T extends AsyncDescriptor>(task: T, ...args: DescriptorArgs<T>): AsyncInstanceForDescriptor<T>;
 }
